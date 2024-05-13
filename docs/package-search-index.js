7fb34a30dfba69aae25236286b64b07f90372c5d
Sunny Tseng <tzuchientseng@gmail.com>
2024-05-13 12:45:26 +0800

    Version 1.6

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"src.lazykits.discreteness"},{"l":"src.lazykits.statistics"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lazykits.discreteness"},{"l":"lazykits.statistics"}];updateSearchResults();
